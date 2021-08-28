fd0aebc43338cbcec131c51590017ab4ff70e34d
yrolland <yann.rolland@iadvize.com>
2021-08-28 13:58:06 +0200

    wip

--- app_test.go
+++ app_test.go
@@ -3,8 +3,10 @@ package kafka_delay_retry
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/stretchr/testify/assert"
 )
 
 // testing main app
@@ -21,10 +23,51 @@ func TestApp(t *testing.T) {
 	}
 
 	app.start()
-
 	produceTestMessages(inputTopic)
-
 	app.stop()
+
+	messages := readMessages(outputTopic, 2000)
+
+	assert.Len(t, messages, 10)
+}
+
+func readMessages(topic string, maxWaitForMessage int) []kafka.Message {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:29092",
+		"group.id":          "test-consumer",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create consumer: %s\n", err))
+	}
+
+	defer func() {
+		fmt.Println("Consumer cleanup")
+		c.Close()
+	}()
+
+	fmt.Printf("Created consumer %v\n", c)
+
+	err = c.SubscribeTopics([]string{topic}, nil)
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to subscribe to topic: %s\n", err))
+	}
+
+	messages := make([]kafka.Message, 0)
+
+	for {
+		msg, err := c.ReadMessage(time.Duration(maxWaitForMessage))
+
+		if err != nil && err.(kafka.Error).Code() == kafka.ErrTimedOut {
+			return messages
+		} else if err == nil {
+			fmt.Printf("Received message: %s\n", string(msg.Value))
+		} else {
+			panic(fmt.Sprintf("Failed to read message: %s\n", err))
+		}
+	}
 }
 
 func produceTestMessages(topic string) {
