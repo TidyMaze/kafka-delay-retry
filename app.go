37a3f7120c34d491ccb74307abe46e0d9ca7b5ab
yrolland <yann.rolland@iadvize.com>
2021-08-28 13:34:16 +0200

    wip

--- app.go
+++ app.go
@@ -2,7 +2,6 @@ package kafka_delay_retry
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"gorm.io/driver/sqlite"
@@ -41,43 +40,6 @@ func (a *KafkaDelayRetryApp) startConsumingMessages() {
 		}
 	}
 
-}
-
-func produceTestMessages(topic string) {
-
-	p, err := kafka.NewProducer(&kafka.ConfigMap{
-		"bootstrap.servers": "localhost:29092",
-		"client.id":         "test-producer",
-	})
-
-	if err != nil {
-		panic(fmt.Sprintf("Failed to create producer: %s\n", err))
-	}
-
-	defer func() {
-		fmt.Println("Producer cleanup")
-		p.Close()
-	}()
-
-	delivery_chan := make(chan kafka.Event, 10000)
-	// produce all numbers from 10 to 20 to kafka topic, prefixed by '-test'
-	for i := 0; i < 10; i++ {
-		fmt.Printf("Producing message to topic %s: %d\n", topic, i)
-		err := p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(fmt.Sprintf("%d-test", i))}, delivery_chan)
-
-		if err != nil {
-			panic(fmt.Sprintf("Failed to produce message: %s\n", err))
-		}
-		// sleep 500ms to simulate a delay
-		time.Sleep(500 * time.Millisecond)
-	}
-	// show end of producer
-	fmt.Println("End of producer")
-
-	remaining := p.Flush(1000)
-	fmt.Printf("%d messages remaining in producer queue\n", remaining)
 }
 
 func main() {
--- app_test.go
+++ app_test.go
@@ -3,7 +3,8 @@ package kafka_delay_retry
 import (
 	"fmt"
 	"testing"
-	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 // testing main app
@@ -18,12 +19,40 @@ func TestApp(t *testing.T) {
 
 	produceTestMessages(inputTopic)
 
-	// show why we are sleeping
-	fmt.Println("Sleeping for 10 seconds")
-	time.Sleep(10 * time.Second)
-
 	app.stop()
+}
 
-	// show end of program
-	fmt.Println("End of kafka test")
+func produceTestMessages(topic string) {
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:29092",
+		"client.id":         "test-producer",
+	})
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create producer: %s\n", err))
+	}
+
+	defer func() {
+		fmt.Println("Producer cleanup")
+		p.Close()
+	}()
+
+	delivery_chan := make(chan kafka.Event, 10000)
+	// produce all numbers from 10 to 20 to kafka topic, prefixed by '-test'
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Producing message to topic %s: %d\n", topic, i)
+		err := p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(fmt.Sprintf("%d-test", i))}, delivery_chan)
+
+		if err != nil {
+			panic(fmt.Sprintf("Failed to produce message: %s\n", err))
+		}
+	}
+	// show end of producer
+	fmt.Println("End of producer")
+
+	remaining := p.Flush(1000)
+	fmt.Printf("%d messages remaining in producer queue\n", remaining)
 }
