6cc58123d81a0b854b4364458c9b2a3b616e0f99
yrolland <yann.rolland@iadvize.com>
2021-09-10 10:43:23 +0200

    wip

--- random_process_app.go
+++ random_process_app.go
@@ -58,7 +58,7 @@ func StartTestApp(ctx context.Context, inputTopic string, outputTopic string, bo
 				panic(fmt.Sprintf("Consumer error: %v (%v)\n", err, msg))
 			}
 
-			isAFailure := rand.Intn(100) < 50
+			isAFailure := rand.Intn(100) < 30
 			if isAFailure {
 				topic := retryTopic
 				fmt.Printf("[RandomProcessApp] Message FAILURE on %s: %s\n", msg.TopicPartition, string(msg.Value))
